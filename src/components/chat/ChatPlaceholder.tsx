1a45181075a1641ebd7eeb13fff9fe870a4db72f
IkigaiLabsXYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2023-08-16 18:10:53 +0200

    commit

--- src/components/chat/ChatPlaceholder.tsx
+++ src/components/chat/ChatPlaceholder.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import AddTokenModal from "./../auth/AddTokenModal";
 import Link from "next/link";
-import GithubStar from "./../misc/GithubStar";
+
 
 type Props = {};
 
@@ -11,8 +11,8 @@ export default function ChatPlaceholder({}: Props) {
       <div className="max-w-3xl p-4 text-center text-primary">
         <h1 className="text-4xl font-medium">GPT-4 Playground</h1>
         <p className="mt-4 text-lg">
-          A ChatGPT clone built with React, Next.js, TailwindCSS, and OpenAI
-          that allows you to play with your GPT-4 api key early!
+          ChatGPT 4.20 built with React, Next.js, TailwindCSS, and OpenAI
+          that allows you to play with your GPT-4 api key.
         </p>
         <div className="m-4 flex items-center justify-center">
           <AddTokenModal />
@@ -27,10 +27,6 @@ export default function ChatPlaceholder({}: Props) {
           </Link>
           !
         </p>
-
-        <div className="p-4">
-          <GithubStar />
-        </div>
       </div>
     </div>
   );
