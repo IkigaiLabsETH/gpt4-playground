61c344542d20babce20fd351da22df4b4f021034
IkigaiLabsXYZ <128307722+IkigaiLabsETH@users.noreply.github.com>
2023-08-17 16:22:28 +0200

    Boska font

--- tailwind.config.js
+++ tailwind.config.js
@@ -22,6 +22,60 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+
+      },
+      fontSize: {
+        0: ["0px", "0px"],
+        h1: [
+          "4rem",
+          {
+            lineHeight: "4.5rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+        h2: [
+          "3rem",
+          {
+            lineHeight: "3.5rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+        h3: [
+          "2.5rem",
+          {
+            lineHeight: "3rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+        h4: [
+          "2rem",
+          {
+            lineHeight: "2.5rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+        h5: [
+          "1.5rem",
+          {
+            lineHeight: "2rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+        h6: [
+          "1.25rem",
+          {
+            lineHeight: "1.5rem",
+            fontWeight: "600",
+            letterSpacing: "-0.01em",
+          },
+        ],
+      },
       textColor,
       backgroundColor,
       colors: {
